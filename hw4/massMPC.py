724e8e474943ce402d33e43f01cb3d2269e21c4a
Mitchell Gehlbach <mitch.gehlbach@gmail.com>
2021-03-07 11:28:10 -0700

    started working on constrained optimizer

--- hw4/massMPC.py
+++ hw4/massMPC.py
@@ -10,7 +10,7 @@ def runoptimization():
     m = 5
     k = 3
     b = 0.5
-    H = 30  # number of points in the horizon
+    H = 100  # number of points in the horizon
 
     # define the state space equations of the form xdot = Ax + Bu
     A = np.array([[0, 1], [-k / m, -b / m]])
@@ -22,7 +22,7 @@ def runoptimization():
     statespace = StateSpace(A, B, C, D)
 
     # The interval of time between each discrete interval
-    Ts = 0.2
+    Ts = 0.05
 
     # creates an object which contains the discretized version of A, B, C, and D as well as a dt
     discrete = c2d(statespace, Ts, method='zoh', prewarp_frequency=None)
@@ -34,13 +34,17 @@ def runoptimization():
         zdot = 0.0  # initial velocity of the mass
         zref = 1.0  # the commanded position for the mass
         x = np.array([[z], [zdot]])  # vectorize the state variables
+        g = np.array([0])
         for i in range(len(u)):
             objhist[i] = z
+            if i != 0:
+                g = np.append(g, 100 - (u[i] - u[i-1])/Ts)
+                g = np.append(g, 100 + (u[i] - u[i-1])/Ts)
             f = f + abs(zref - z)
             x_next = np.matmul(discrete.A, x) + np.matmul(discrete.B, np.array([[u[i]]]))
             z = x_next[0]
             x = x_next
-        return f[0, 0]
+        return f[0, 0], g
 
     ulast = []
     flast = []
@@ -49,14 +53,14 @@ def runoptimization():
     def obj(u, discrete):
         nonlocal ulast, flast, glast
         if not np.array_equal(u, ulast):
-            flast = objcon(u, discrete)
+            flast, glast = objcon(u, discrete)
             ulast = u
         return flast
 
     def con(u):
         nonlocal ulast, flast, glast
         if not np.array_equal(u, ulast):
-            flast = objcon(u, discrete)
+            flast, glast = objcon(u, discrete)
             ulast = u
         return glast
 
@@ -64,7 +68,7 @@ def runoptimization():
     constraints = {'type': 'ineq', 'fun': con}
     options = {'disp': True}
 
-    res = minimize(obj, u0, args=discrete, bounds=Bounds(-50.0, 50.0), options=options, method='slsqp')
+    res = minimize(obj, u0, args=discrete, bounds=Bounds(-10.0, 10.0), constraints=constraints, options=options, method='slsqp')
     # print("x = ", res.x)
     # print('f = ', res.fun)
     # print(res.success)
